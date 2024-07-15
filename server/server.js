affb3292c5fdfa33b96e5b183b7c9af1e060d582
jmanq <jakeforte70@gmail.com>
2024-07-15 15:57:50 -0400

    react basically done

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const collectionName = process.env.MONGO_DB_COLLECTION;
+const cartCollection = process.env.CART_COLLECTION;
+const orderCollection = process.env.ORDERS_COLLECTION;
 
 const app = express();
 app.use(cors()); // Enable CORS for all routes
@@ -43,13 +45,64 @@ app.post('/pizza/search', async (req, res) => {
     }
 });
 
-app.put("/cart/addProduct", (req, res) => {
-    if (req.body === undefined) {
-        res.statusCode = 500;
-        res.end();
-        return;
+app.post("/cart/addProduct", async (req, res) => {
+    try {
+        const product  = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(cartCollection);
+        const result = await collection.insertOne(product);
+        res.status(201).send(`{"_id":"${result.insertedId}"}`);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding pizza');
     }
-    res.end()
+});
+
+app.get("/cart/getCart", async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(cartCollection);
+        const cart = await collection.find({}).toArray();
+        res.json(cart);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding pizza');
+    }
+});
+
+app.delete('/cart/getCart/:Pizza_ID', async (req, res) => {
+    try {
+        const { Pizza_ID } = req.params;
+        console.log("ID: ", parseInt(Pizza_ID));
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(cartCollection);
+        const result = await collection.deleteOne({ Pizza_ID:parseInt(Pizza_ID) });
+        if (result.deletedCount === 1) {
+            res.status(200).send('Pizza deleted successfully');
+        } else {
+            res.status(404).send('Pizza not found');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error deleting Pizza');
+    }
+});
+
+app.post("/cart/checkout", async (req, res) => {
+    try {
+        const product  = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(orderCollection);
+        const result = await collection.insertOne(product);
+        res.status(201).send(`{"_id":"${result.insertedId}"}`);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding pizza');
+    }
 });
 
 app.listen(PORT, () => {
